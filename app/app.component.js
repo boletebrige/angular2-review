a0c2a63c5274708e4adf1a583403192f69012c67
boletebrige <josip.ravas.broj@gmail.com>
2017-08-09 00:44:54 +0200

    passing data from a class to template 🚚

--- app/app.component.js
+++ app/app.component.js
@@ -9,11 +9,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.message = 'Hello';
+        this.users = [
+            { id: 25, name: 'Josip', username: 'boletebrige' },
+            { id: 26, name: 'John', username: 'snow' },
+            { id: 27, name: 'Someone', username: 'Nobody' },
+        ];
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n        <div class=\"jumbotron text-center\">\n            <h1>Welcome to Our App!</h1>\n        </div>\n    ",
+            template: "\n        <header>\n            <nav class=\"navbar navbar-inverse\">\n                <div class=\"navbar-header\">\n                    <a href=\"/\" class=\"navbar-brand\">My Angular 2 App!</a>\n                </div>\n            </nav>\n        </header>\n        <main>\n            <div class=\"jumbotron text-center\">\n                <h1>Welcome to Our App!</h1>\n                <p>{{ message }}</p>\n            </div>\n            <p>The user is</p>\n            <ul>\n                <li *ngFor=\"let user of users\">{{ user.name }} ({{ user.username }})</li>\n            </ul>\n        </main>\n        <footer class=\"text-center\">\n            Copyleft \u2763 2016\n        </footer>\n    ",
             styles: ["\n        .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }\n    "]
         })
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,37 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <div class="jumbotron text-center">
-            <h1>Welcome to Our App!</h1>
-        </div>
+        <header>
+            <nav class="navbar navbar-inverse">
+                <div class="navbar-header">
+                    <a href="/" class="navbar-brand">My Angular 2 App!</a>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="jumbotron text-center">
+                <h1>Welcome to Our App!</h1>
+                <p>{{ message }}</p>
+            </div>
+            <p>The user is</p>
+            <ul>
+                <li *ngFor="let user of users">{{ user.name }} ({{ user.username }})</li>
+            </ul>
+        </main>
+        <footer class="text-center">
+            Copyleft ❣ 2016
+        </footer>
     `,
     styles: [`
         .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
     `]
 
 })
-export class AppComponent {}
+export class AppComponent {
+    message = 'Hello';
+    users = [
+        { id: 25, name: 'Josip', username: 'boletebrige' },
+        { id: 26, name: 'John', username: 'snow' },
+        { id: 27, name: 'Someone', username: 'Nobody' },
+    ]
+}
